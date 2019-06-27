59375f09df4dcee93787eb2726f5529176ef65e1
Shubham <Shubham.Chouhan@lntinfotech.com>
2019-06-27 14:05:04 +0530

    New changes

---
+++ lib/pages/pageFirst.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class PageFirst extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("First Page"),
+      ),
+      body: Text("First Page"),
+    );
+  }
+}
